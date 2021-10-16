4008eb6fd133cdb35da061b6222f9f566085c6bd
Christian David Garcia G <cdavid.ggarcia@gmail.com>
2021-10-16 20:05:29 +0200

    feat: add enviroment variables

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -2,28 +2,18 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import  'firebase/auth';
 
-let firebaseConfig = {};
 
-if (process.env.NODE_ENV === 'test') {
-  firebaseConfig = {
-    apiKey: "AIzaSyD5cQ84gDZlQKXYq1_jNpRNv5RKzeMrIGY",
-    authDomain: "testing-dd6df.firebaseapp.com",
-    projectId: "testing-dd6df",
-    storageBucket: "testing-dd6df.appspot.com",
-    messagingSenderId: "592492821533",
-    appId: "1:592492821533:web:62fd0238a9f25e6946daf4"
-  };
-} else { // Dev & Prod
-  firebaseConfig = {
-    apiKey: "AIzaSyDREDOeYFZuRn_05BjDwuBzBbED_WFNGEA",
-    authDomain: "react-app-73d3f.firebaseapp.com",
-    projectId: "react-app-73d3f",
-    storageBucket: "react-app-73d3f.appspot.com",
-    messagingSenderId: "626268814484",
-    appId: "1:626268814484:web:7321b787c5b9f63ca8c881"
-  };
-}
+console.log('cons', process.env )
 
+
+const firebaseConfig = {
+  apiKey: process.env?.REACT_APP_API_KEY,
+  authDomain: process.env?.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env?.REACT_APP_projectId,
+  storageBucket: process.env?.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env?.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env?.REACT_APP_APP_ID
+};
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
