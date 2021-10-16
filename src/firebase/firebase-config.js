f0c7b2869f78f9ec16afa3841f1ed070cf4354e8
Christian David Garcia G <cdavid.ggarcia@gmail.com>
2021-10-16 20:05:59 +0200

    test: notes.test

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -2,10 +2,6 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import  'firebase/auth';
 
-
-console.log('cons', process.env )
-
-
 const firebaseConfig = {
   apiKey: process.env?.REACT_APP_API_KEY,
   authDomain: process.env?.REACT_APP_AUTH_DOMAIN,
--- src/tests/actions/notes.test.js
+++ src/tests/actions/notes.test.js
@@ -1,19 +1,27 @@
+ /** * @jest-environment node */
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { startNewNote } from '../../actions/notes';
+import { startLoadingNotes, startNewNote, startSaveNote } from '../../actions/notes';
 import { db } from '../../firebase/firebase-config';
 import { types } from '../../types/types';
 
 const middlewares = [thunk]
 const mockStore = configureStore(middlewares)
 
-const store = mockStore({
+const initState = {
   auth: {
     uid: 'testUID'
   }
-});
+}
+
+let store = mockStore(initState);
 
 describe('notes.js', () => {
+
+  beforeEach(() => {
+    store = mockStore(initState)
+  })
+
   test('should create new note (startNewNote)', async () => {
     await store.dispatch(startNewNote());
     const uid = store.getState().auth.uid;
@@ -43,6 +51,43 @@ describe('notes.js', () => {
     actions.forEach(async(action) => {
       await db.doc(`${uid}/journal/notes/${action?.payload?.id}`).delete()
     });
+
   })
 
+  test('should load notes (startLoadingNotes)', async() => {
+    const uid = store.getState().auth.uid;
+    await store.dispatch(startLoadingNotes(uid))
+    const actions = store.getActions()
+    const expected = {
+      id: expect.any(String),
+      title: expect.any(String),
+      body: expect.any(String),
+      date: expect.any(Number),
+    }
+
+    expect(actions[0]).toEqual({
+      type: types.notesLoad,
+      payload: expect.any(Array)
+    })
+    expect(actions[0].payload[0]).toMatchObject(expected)
+  })
+
+  test('should update note (startSaveNote)', async() => {
+    const uid = store.getState().auth.uid;
+
+    const note = {
+      id: 'B06eTstF8i8lOKR6esjE',
+      title: 'title',
+      body: 'body'
+    }
+
+    await store.dispatch(startSaveNote(note))
+    const actions = store.getActions()
+
+    expect(actions[0].type).toBe(types.notesUpdated)
+    const docRef = await db.doc(`${uid}/journal/notes/${note.id}`).get()
+
+    expect(docRef.data().title).toBe(note.title)
+    expect(docRef.data().body).toBe(note.body)
+  })
 })
